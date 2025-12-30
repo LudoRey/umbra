feat(memory): correct computation of memory requirements

umbra/integration/memory.py
@@ -3,15 +3,17 @@ import psutil
 
 from umbra.common.terminal import cprint
 
-def compute_stack_memory_requirements(num_images, shape, dtype=np.float32):
-    '''See docstring notes of umbra.integration.rejection.outlier_rejection for details.'''
-    stack_memory = np.prod(shape) * num_images * (np.dtype(dtype).itemsize)
-    rejection_memory = np.prod(shape) * (num_images * 2 + (np.dtype(dtype).itemsize) * 2)
-    return stack_memory + rejection_memory
+def compute_stack_memory_requirements(num_images, height, width, num_channels, byte_per_pixel=4):
+    '''Peak memory usage during stacking. It is attained during outlier rejection.'''
+    stack_memory = height * width * num_channels * num_images * byte_per_pixel
+    weights_memory = height * width * num_images * byte_per_pixel
+    output_memory = height * width * num_channels * byte_per_pixel * 2
+    peak_rejection_memory = height * width * num_channels * (num_images * 2 + byte_per_pixel * 2) # see outlier_rejection notes
+    return stack_memory + weights_memory + output_memory + peak_rejection_memory
 
 def compute_rows_ranges_for_stack(num_images, shape, max_mem, dtype=np.float32):
-    H = shape[0]
-    required_stack_mem = compute_stack_memory_requirements(num_images, shape, dtype)
+    height, width, num_channels = shape
+    required_stack_mem = compute_stack_memory_requirements(num_images, height, width, num_channels, np.dtype(dtype).itemsize)
     n_chunks = int(np.ceil(required_stack_mem / max_mem))
     chunk_mem = required_stack_mem / n_chunks
 
@@ -21,14 +23,14 @@ def compute_rows_ranges_for_stack(num_images, shape, max_mem, dtype=np.float32):
         cprint(f"The entire stack fits into memory. Approximate memory usage: {chunk_mem / 1000000:.2f} MB.")
 
     # Divide into chunks as evenly as possible, with the remainder distributed among the first chunks
-    chunk_sizes = [(H // n_chunks) + (1 if i < H % n_chunks else 0) for i in range(n_chunks)]
+    chunk_sizes = [(height // n_chunks) + (1 if i < height % n_chunks else 0) for i in range(n_chunks)]
 
     rows_ranges = []
-    start = 0
+    row_start = 0
     for size in chunk_sizes:
-        end = start + size
-        rows_ranges.append((start, end))
-        start = end
+        row_end = row_start + size
+        rows_ranges.append((row_start, row_end))
+        row_start = row_end
     return rows_ranges
 
 def get_available_memory():